b3f905f578344a04f4c9711a2b96724b62295e5a
atashaikh2 <ataurrehman.shaikh@gmail.com>
2023-06-05 08:59:25 -0600

    Create for_each.tf

---
+++ terraform/for_each.tf
@@ -0,0 +1,24 @@
+locals {
+  prefix = "kevin-code-sec"
+  buckets = [
+    "test-code-sec-a",
+    "test-code-sec-b",
+  ]
+  test_buckets = [
+    "test-bucket1",
+    "test-bucket2"
+  ]
+  additional_tags = {
+    Env                  = "DEV"
+    Point_of_Contact     = "CloudSec"
+    Managed_by_Terraform = true
+  }
+}
+
+
+resource "aws_s3_bucket" "this" {
+  for_each = toset(local.test_buckets)
+  bucket   = "${local.prefix}-${each.key}"
+  tags     = local.additional_tags
+}
+
