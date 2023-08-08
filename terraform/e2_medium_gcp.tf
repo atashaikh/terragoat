fa4a75f6d90f5438385d98ce5693d29ca37d7754
atashaikh2 <ataurrehman.shaikh@gmail.com>
2023-08-08 14:33:44 -0600

    Create e2_medium_gcp.tf

---
+++ terraform/e2_medium_gcp.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "your-project-id"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "example_instance" {
+  name         = "example-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"  # Choose an appropriate image
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  tags = ["example-tag"]
+}
